7fa9d6aaa05dc96dd641038eae2a0a8c3fcebb7d
fethi <fethi.meziane.fm@gmail.com>
2023-11-11 13:00:11 +0100

    connect the mongoDB

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express');
-
-
-const booksRouter = require('./Router/bookRouter')
-const authorsRouter = require('./Router/authorsRouter')
+const express = require("express");
+const mongoos = require("mongoose");
+const booksRouter = require("./Router/bookRouter");
+const authorsRouter = require("./Router/authorsRouter");
 const port = 3000;
+
+mongoos
+.connect("mongodb://127.0.0.1/BookStorDb")
+.then(() => console.log("conncted To Mongo DB"))
+.catch((error) => console.log("conncetion is failde ", error));
+
+
+
 // Init App
 const app = express();
-//Apply middliwares
-app.use(express.json())
 
+
+
+
+//Apply middliwares
+app.use(express.json());
 
 //Routes
-app.use('/api/books',booksRouter)
-app.use('/api/authors',authorsRouter)
+app.use("/api/books", booksRouter);
+app.use("/api/authors", authorsRouter);
 
 // runnunig server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
