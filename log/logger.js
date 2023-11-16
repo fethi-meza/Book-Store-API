a43c6c810c78983f0ca191eec36613d127d7c0a7
fethi <fethi.meziane.fm@gmail.com>
2023-11-16 15:33:03 +0100

    env

--- log/logger.js
+++ log/logger.js
@@ -1,6 +1,12 @@
-const logger =()=>{
-    console.log("hello word")
+const logger =(req,res,next)=>{
+    console.log(`${req.method} ${req.protocol}:// ${req.get('host')} ${req.originalUrl}`)
+    next()
 }
 
 
-module.exports ={logger}
+const ErrorHendler = (err,req,res,next)=>{
+    const statuseCode =res.statuse === 200 ? 500 : res.statuseCode
+    res.statuse(statuseCode).json({message : err.message})
+}
+
+module.exports ={logger , ErrorHendler}
--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const mongoos = require("mongoose");
 const booksRouter = require("./Router/bookRouter");
 const authorsRouter = require("./Router/authorsRouter");
-const port = 3000;
+const {logger, ErrorHendler}= require('./log/logger')
+
+//config env
+require('dotenv').config()
+
+
+const port = 3000 || process.env.PORT;
+
 //connection Db
 mongoos
-.connect("mongodb://127.0.0.1/BookStorDb")
+.connect(process.env.MONGO_URI)
 .then(() => console.log("conncted To Mongo DB"))
 .catch((error) => console.log("conncetion is failde ", error));
 
@@ -20,9 +27,18 @@ const app = express();
 //Apply middliwares
 app.use(express.json());
 
+app.use(logger)
+
 //Routes
 app.use("/api/books", booksRouter);
 app.use("/api/authors", authorsRouter);
+app.use((req,res, next)=>{
+const error = new Error(`not found  -${req.originalUrl}`)
+res.status(404)
+next(error)
+})
+app.use(ErrorHendler)
 
 // runnunig server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(` server running in mdoe ${process.env.NODE_ENV} on port ${port}!`));
+ 
