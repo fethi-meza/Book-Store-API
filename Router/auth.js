e526280d6e88a7b8900e7cd4e532ac1092accf89
fethi <fethi.meziane.fm@gmail.com>
2023-11-20 16:56:44 +0100

    Register auth

--- Router/auth.js
+++ Router/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const {User,ValidateLoginUser,ValidateRegisterUser}= require('../Models/User')
 
 
 /**
@@ -10,15 +10,30 @@ const router = express.Router()
  * @access public
  */
 router.post('/register', async(req,res)=>{
-
 try {
-    
+    const {error} = ValidateRegisterUser(req.body);
+    if(error){
+        res.status(500).json({message : error.details[0].message})
+    }
+let user = await User.findOne({email : req.body.email}); 
+if(user){
+res.status(400).json({message:"this user is Already Registered "})
+}
 
+ user = new User ({
+  eamil :req.body.eamil ,
+  username :req.body.username ,
+  password :req.body.password ,
+  isAdmine :req.body.isAdmine ,
+ })
+
+ const result = await user.save();
+ res.status(201).json(result)
+ 
 } catch (error) {
     console.log(error)
     res.status(500).json({message : "somthing went wrong ! "})
 }
-
 })
 
 
